77c2ac604414cda5fb984113d806af628845d0c4
EmptyEXOT <sadullaev.alim@mail.ru>
2024-06-14 18:18:59 +0300

    index file

---
+++ client/src/index.tsx
@@ -0,0 +1,25 @@
+import {createRoot} from "react-dom/client";
+import React, {StrictMode} from "react";
+import '@/root/global.scss'
+import {RouterProvider} from "react-router-dom";
+import {router} from "@/shared/router/router";
+import {StoreProvider} from "@/shared/store/StoreProvider";
+import {ThemeProvider} from "@/shared/theme/ThemeProvider";
+
+
+const node = document.getElementById('root');
+if (!node) {
+    throw new Error(`Node with id 'root' doesn't exist`)
+}
+
+createRoot(node)
+    .render(
+        <StrictMode>
+            <ThemeProvider>
+                <StoreProvider>
+                    <RouterProvider router={router}/>
+                </StoreProvider>
+            </ThemeProvider>
+        </StrictMode>
+    )
+
