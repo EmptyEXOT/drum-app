def9d4ce4206c41a6812675e4f62c4e9333160ab
EmptyEXOT <sadullaev.alim@mail.ru>
2024-06-14 18:16:13 +0300

    sidebar widget init

---
+++ client/src/widgets/Sidebar/ui/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classNames from "classnames";
+
+export const Sidebar = () => {
+    return (
+        <div className={classNames('w-[20vw] h-screen bg-amber-50')}>
+            menu
+        </div>
+    );
+};
