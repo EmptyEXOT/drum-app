0e3a4e2e69f2d8354b1c7d473e4450dfecb75373
EmptyEXOT <sadullaev.alim@mail.ru>
2024-06-19 11:24:22 +0300

    shine size, story and colors

--- client/src/shared/ui/Shine/Shine.tsx
+++ client/src/shared/ui/Shine/Shine.tsx
@@ -3,12 +3,10 @@ import classNames from "classnames";
 import cls from './Shine.module.scss'
 
 interface ShineProps {
-    children?: ReactNode,
     className?: string,
 }
 
 export const Shine: FC<ShineProps> = ({
-                                          children,
                                           className,
                                           ...props
                                       }) => {
