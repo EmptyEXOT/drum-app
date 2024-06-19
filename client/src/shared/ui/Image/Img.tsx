7cede6798360f6df311d70dd80dc00928e21e8fd
EmptyEXOT <sadullaev.alim@mail.ru>
2024-06-19 11:22:37 +0300

    Image component

---
+++ client/src/shared/ui/Image/Img.tsx
@@ -0,0 +1,27 @@
+import React, {FC, ReactNode} from 'react';
+import classNames from "classnames";
+import {Typo} from "@/shared/ui/Typo/Typo";
+
+interface ImgProps {
+    src: string,
+    alt: string,
+    className?: string,
+}
+
+export const Img: FC<ImgProps> = ({
+                                      src,
+                                      alt,
+                                      className,
+                                      ...props
+                                  }) => {
+    return (
+        <div className={classNames(
+            'z-30 relative',
+            className,
+        )}>
+            <img className={classNames(
+                'object-contain h-full w-full'
+            )} src={src as string} alt={alt}/>
+        </div>
+    );
+};
