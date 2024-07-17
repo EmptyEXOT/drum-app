369265f5bb4d10e1214ac8883041300525953373
EmptyEXOT <sadullaev.alim@mail.ru>
2024-07-17 21:43:40 +0300

    entities to global state

--- client/src/shared/store/types.ts
+++ client/src/shared/store/types.ts
@@ -1,3 +1,7 @@
-export interface StateSchema {
+import {SheetModel} from "@/entities/Sheet/model/slice/types";
+import {TrackModel} from "@/entities/Track/model/slice/types";
 
+export interface StateSchema {
+    sheet: SheetModel,
+    track: TrackModel,
 }
