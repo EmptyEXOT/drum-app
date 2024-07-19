7b38764552c2d06837f60622b32b8ef78af9a1de
EmptyEXOT <sadullaev.alim@mail.ru>
2024-07-19 18:55:12 +0300

    Kicks to sheet slice

--- client/src/entities/Sheet/model/slice/types.ts
+++ client/src/entities/Sheet/model/slice/types.ts
@@ -1,11 +1,13 @@
 import {NoteBase} from "@/entities/Note/model/slice/types";
 import {NoteType} from "@/pages/AlphabetAppPage/types";
+import {IKick} from "@/pages/AlphabetAppPage/Kick";
 
 export interface SheetModel {
     signature: [number, number],
     author: string,
     title: string,
-    bars: Note[][]
+    bars: Note[][],
+    kicks: IKick[][],
 }
 
 export interface Note {
